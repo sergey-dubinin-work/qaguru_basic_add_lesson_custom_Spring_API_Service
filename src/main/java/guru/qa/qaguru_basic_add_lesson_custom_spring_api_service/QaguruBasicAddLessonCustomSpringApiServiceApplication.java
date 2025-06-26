6e6e1f9765775d1dfba15bf168a4c1570612d5cc
Sergey Dubinin <s.dubinin@dex-it.ru>
2025-06-26 17:29:43 +0300

    init project

---
+++ src/main/java/guru/qa/qaguru_basic_add_lesson_custom_spring_api_service/QaguruBasicAddLessonCustomSpringApiServiceApplication.java
@@ -0,0 +1,13 @@
+package guru.qa.qaguru_basic_add_lesson_custom_spring_api_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QaguruBasicAddLessonCustomSpringApiServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QaguruBasicAddLessonCustomSpringApiServiceApplication.class, args);
+    }
+
+}
