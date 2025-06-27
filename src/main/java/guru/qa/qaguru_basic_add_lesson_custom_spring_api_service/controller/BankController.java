e89ab3b08789ef2ccc243ab27685afbc04172665
Sergey Dubinin <s.dubinin@dex-it.ru>
2025-06-27 12:02:35 +0300

    add swagger dependency and custom description

--- src/main/java/guru/qa/qaguru_basic_add_lesson_custom_spring_api_service/controller/BankController.java
+++ src/main/java/guru/qa/qaguru_basic_add_lesson_custom_spring_api_service/controller/BankController.java
@@ -3,6 +3,8 @@ package guru.qa.qaguru_basic_add_lesson_custom_spring_api_service.controller;
 import guru.qa.qaguru_basic_add_lesson_custom_spring_api_service.domain.LoginInfo;
 import guru.qa.qaguru_basic_add_lesson_custom_spring_api_service.domain.UserInfo;
 import guru.qa.qaguru_basic_add_lesson_custom_spring_api_service.exception.InvalidUserNameException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Date;
 
 @RestController
+@Tag(name = "BankController", description = "Bank operations")
 public class BankController {
 
     @PostMapping("user/login")
+    @Operation(summary = "Login user")
     public UserInfo doLogin(@RequestBody LoginInfo loginInfo){
         if (loginInfo.getUserName().equals("akado")){
             return UserInfo.builder()
