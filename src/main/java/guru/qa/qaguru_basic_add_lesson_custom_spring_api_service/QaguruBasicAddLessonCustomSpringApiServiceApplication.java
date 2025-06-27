9296c637c8a5913d3a482fda0ec8968902eb06a3
Sergey Dubinin <s.dubinin@dex-it.ru>
2025-06-27 12:03:01 +0300

    fix

--- src/main/java/guru/qa/qaguru_basic_add_lesson_custom_spring_api_service/QaguruBasicAddLessonCustomSpringApiServiceApplication.java
+++ src/main/java/guru/qa/qaguru_basic_add_lesson_custom_spring_api_service/QaguruBasicAddLessonCustomSpringApiServiceApplication.java
@@ -2,7 +2,6 @@ package guru.qa.qaguru_basic_add_lesson_custom_spring_api_service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
--- src/main/java/guru/qa/qaguru_basic_add_lesson_custom_spring_api_service/domain/LoginInfo.java
+++ src/main/java/guru/qa/qaguru_basic_add_lesson_custom_spring_api_service/domain/LoginInfo.java
@@ -1,10 +1,14 @@
 package guru.qa.qaguru_basic_add_lesson_custom_spring_api_service.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginInfo {
     private String userName;
     private String password;
